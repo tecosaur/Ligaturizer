60a2a49d42ce6af270d6cd5b7e489ef69ebc63e1
tecosaur <20903656+tecosaur@users.noreply.github.com>
2019-03-24 20:32:50 +0800

    Change example

--- ligatures.py
+++ ligatures.py
@@ -1,1 +1,4 @@
-ligatures = [{"chars": ["\\", "i", "n"], "firacode_ligature_name": 8712}]
+ligatures = [
+    {"chars":[97], "firacode_ligature_name": None},
+    {"chars": ["\\", "i", "n"], "firacode_ligature_name": 97}
+]
